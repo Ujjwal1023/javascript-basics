ed788458aec90199a3d6c819b94b67252bb895c6
Ujjwal1023 <kumarujjwal471@gmail.com>
2023-09-02 10:48:06 +0530

    js basics added

---
+++ script.js
@@ -0,0 +1,60 @@
+console.log("javascript dfjhhihiu   s");
+console.log("text");
+console.warn(1);
+console.log(true); 
+let firstname="ujjwal"
+console.log("name:"+firstname);
+let radius=5;
+let c=2*Math.PI*radius;
+console.log("circumference of circle:"+c);
+let b=5;
+console.log(b>10);
+console.log(typeof b);
+let address=null;
+console.log(address ,typeof address);
+
+
+let fruits=["apple","banana","singing"];
+console.log(fruits[2]);
+
+let currentDate=new Date;
+console.log(currentDate.getDate(),currentDate.getDay(),currentDate.getFullYear());
+
+let num1=100;
+let num2=50;
+
+console.log(num1*num2);
+
+let day=3;
+let dayname;
+
+switch(day){
+    case 1:
+        dayname="monday";
+        break;
+        
+
+    case 2:
+        dayname="tuesday";
+        break;
+
+
+    case 3:
+        dayname="wednesday";
+        break;
+
+
+    default:
+        dayname="incorrect";
+
+}
+let username=null;
+let displayname=null??"undefined user";
+console.log(displayname)
+function sum(a,b){
+    return a*b;
+}
+
+let     mysum=sum(5,10000000);
+console.log(mysum)
+
